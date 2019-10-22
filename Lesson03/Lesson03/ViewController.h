9cf828243bbc931f4defd2621b885677f8b77f2b
Maksim Nosov <ozolc@yandex.ru>
2019-10-22 20:39:36 +0300

    Lesson03. Initial commit.

---
+++ Lesson03/Lesson03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson03
+//
+//  Created by Maksim Nosov on 22/10/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lesson03/Lesson03/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Lesson03
+//
+//  Created by Maksim Nosov on 22/10/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.blueColor; // TODO: Proof we have loaded
+}
+
+
+@end
