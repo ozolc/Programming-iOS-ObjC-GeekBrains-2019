b6edd24ff8010127ac9ff3dcf2b7680b9a1fbf16
Maksim Nosov <ozolc@yandex.ru>
2019-10-24 21:23:49 +0300

    5. Разработка под iOS на Objective-C. Поиск в приложении, UIImagePickerController, UICollectionView

---
+++ Lesson05/Lesson05/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson05
+//
+//  Created by Maksim Nosov on 24/10/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson05/Lesson05/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Lesson05
+//
+//  Created by Maksim Nosov on 24/10/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+
+@end
