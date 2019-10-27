f7d25ddb121825aa77e154cece4d7e58dfa9a497
Maksim Nosov <ozolc@yandex.ru>
2019-10-27 08:33:57 +0300

    Lesson07. Разработка под iOS на Objective-C. Анимация и визуальные эффекты, UIPageViewController и уведомления.

---
+++ Lesson07/Lesson07/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson07
+//
+//  Created by Maksim Nosov on 27.10.2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lesson07/Lesson07/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Lesson07
+//
+//  Created by Maksim Nosov on 27.10.2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+
+@end
